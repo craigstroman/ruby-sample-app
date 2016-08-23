7c6b28480918a60ddf283fdf0b0d6ef1f0d3c6d1
craigstroman <cstroman@gmail.com>
2016-08-23 14:56:28 -0400

    Chapter 11 - Account Activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,21 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
     
   	if user && user.authenticate(params[:session][:password])
-  		# Log user in and redirect user to welcome page.
-  		log_in user
-            params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		redirect_back_or user
+        if user.activated?
+        # Log user in and redirect user to welcome page.
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "Account not activated."
+        message += "Check your emailfor the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
   	else 
-  		# Show an error message.
-  		flash.now[:danger] = "Invalid email/password combination."
-		render 'new'
+        # Show an error message.
+        flash.now[:danger] = "Invalid email/password combination."
+        render 'new'
   	end 
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,7 @@ module SessionsHelper
 			@current_user ||= User.find_by(id: session[:user_id])
 		elsif ( user_id = cookies.signed[:user_id] )
 			user = User.find_by(id: user_id)
-			if user && user.authenticated?( cookies[:remember_token] )
+			if user && user.authenticated?(:remember, cookies[:remember_token])
 				log_in user
 				@current_user = user
 			end
@@ -27,8 +27,6 @@ module SessionsHelper
 	def current_user?(user)
 		user == current_user
 	end
-
-
 
 	# Returns true if the user is logged in, false otherwise.
 	def logged_in?
