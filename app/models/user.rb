ba54827f45f44b2834ee810474fddae00202c9aa
craigstroman <cstroman@gmail.com>
2016-09-26 14:57:04 -0400

    Chapter 14 - Following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,14 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 	has_many :microposts, dependent: :destroy
-
+	has_many :active_relationships, class_name: "Relationship",
+					foreign_key: "follower_id",
+					dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",
+					     foreign_key: "followed_id",
+					     dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
 	# Returns the hash digest pf the given string.
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -71,6 +78,27 @@ class User < ApplicationRecord
 	# See "Following users" for the full implementation.
 	def feed
 		Micropost.where("user_id = ?", id)
+	end
+
+	# Follows a user.
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+
+	# Unfollows a user
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	# Returns true if the current user is following the other user
+	def following?(other_user)
+		following.include?(other_user)
+	end
+
+	# Returns a user's status feeed
+	def feed
+		following_ids = "SELECT followed_id from relationships where follower_id = :user_id"
+		Micropost.where("user_id IN (:following_ids) OR user_id = :user_id", following_ids: following_ids, user_id: id)
 	end
 
 	private
